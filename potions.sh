766ad4f46654d4a6d08afdc22732767665c8498f
mattmasel <94439034+mattmasel@users.noreply.github.com>
2024-01-11 14:30:50 +0800

    removed closed source installations

--- potions.sh
+++ potions.sh
@@ -142,10 +142,6 @@ else
     echo "[+] Subfinder already installed"
 fi
 
-# Install Closed Source Programs
-
-# Burp
-
 # Add time and date to bash prompt
 
 echo 'export PS1="-[\[$(tput sgr0)\]\[\033[38;5;10m\]\d\[$(tput sgr0)\]-\[$(tput sgr0)\]\[\033[38;5;10m\]\t\[$(tput sgr0)\]]-[\[$(tput sgr0)\]\[\033[38;5;214m\]\u\[$(tput sgr0)\]💀\[$(tput sgr0)\]\[\033[38;5;196m\]\h\[$(tput sgr0)\]]-\n-[\[$(tput sgr0)\]\[\033[38;5;33m\]\w\[$(tput sgr0)\]]\\$ \[$(tput sgr0)\]"' >> ~/.bashrc
